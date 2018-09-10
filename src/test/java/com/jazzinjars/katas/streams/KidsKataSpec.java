d44231c62ac2134ed4666c9c2737750138b2b6b6
jazzinjars <hectorardila@gmail.com>
2018-09-10 10:57:29 +0200

    Fix some test issues on Kata

--- src/test/java/com/jazzinjars/katas/streams/KidsKataSpec.java
+++ src/test/java/com/jazzinjars/katas/streams/KidsKataSpec.java
@@ -2,6 +2,9 @@ package com.jazzinjars.katas.streams;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static com.jazzinjars.katas.streams.Kids.getKidNames;
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -10,7 +13,8 @@ public class KidsKataSpec extends KataBaseSpec {
     @Test
     public void get_kid_name_should_return_names_of_younger_than_18() {
         Person arya = new Person("Arya", 5);
-        personList.add(arya);
+        List<Person> people = new ArrayList<>(personList);
+        people.add(arya);
 
         assertThat(getKidNames(personList))
                 .contains("Sansa", "Arya")
