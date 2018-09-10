10edd524a1c22d25709738bd03e18a9b5faddb05
jazzinjars <hectorardila@gmail.com>
2018-09-10 10:55:36 +0200

    Fix some test issues on Kata

--- src/main/java/com/jazzinjars/katas/streams/Sum.java
+++ src/main/java/com/jazzinjars/katas/streams/Sum.java
@@ -14,7 +14,7 @@ public class Sum {
 
     public static int calculate(List<Integer> people) {
         return people.stream()                                          // Convert collection to Stream
-                .reduce(0, (total, number) -> total + number);  // Sum elements with 0 as starting value
+                .reduce(0, (person1, person2) -> person1 + person2);  // Sum elements with 0 as starting value
     }
 
 }
--- src/test/java/com/jazzinjars/katas/streams/KidsKataSpec.java
+++ src/test/java/com/jazzinjars/katas/streams/KidsKataSpec.java
@@ -2,6 +2,9 @@ package com.jazzinjars.katas.streams;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static com.jazzinjars.katas.streams.Kids.getKidNames;
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -10,9 +13,10 @@ public class KidsKataSpec extends KataBaseSpec {
     @Test
     public void get_kid_name_should_return_names_of_younger_than_18() {
         Person arya = new Person("Arya", 5);
-        personList.add(arya);
+        List<Person> people = new ArrayList<>(personList);
+        people.add(arya);
 
-        assertThat(getKidNames(personList))
+        assertThat(getKidNames(people))
                 .contains("Sansa", "Arya")
                 .doesNotContain("Cersei", "Catelyn");
     }
